5d231f114314e962b992f90b422139582643bf26
Marcos Franco <marcosfrc013@gmail.com>
2021-07-12 13:59:59 -0300

    Se arreglo error de editar usuarios

--- modUsuarios/guardarPersonaConUsuario.php
+++ modUsuarios/guardarPersonaConUsuario.php
@@ -1,7 +1,6 @@
-  <?php
+<?php
 
-  include_once '../conexion/conexion.php';
-
+include_once '../conexion/conexion.php';
 
 $nombrePersona = $_POST['nombrePersona'];
 $apellidoPersona = $_POST['apellidoPersona'];
@@ -9,43 +8,46 @@ $dniPersona = $_POST['dniPersona'];
 $telefonoPersona = $_POST['telefonoPersona'];
 $correoPersona = $_POST['correoPersona'];
 
-
 $nombreUsuario = $_POST['nombreUsuario'];
 $contrasenaUsuario = $_POST['contrasena'];
 $tipoUsuario = $_POST['tipoUsuario'];
 
-if (!empty($nombrePersona) && !empty($apellidoPersona) && !empty($dniPersona) && !empty($telefonoPersona) && !empty($correoPersona) && !empty($nombreUsuario) && !empty($contrasenaUsuario) && !empty($tipoUsuario)) {
-    
+if ( !empty( $nombrePersona ) && !empty( $apellidoPersona ) && !empty( $dniPersona ) && !empty( $telefonoPersona ) && !empty( $correoPersona ) && !empty( $nombreUsuario ) && !empty( $contrasenaUsuario ) && !empty( $tipoUsuario ) ) {
 
-
-  // inserccion de los datos en la tabla de personas
-  $sqlInsertarPersona = "INSERT INTO `Personas`(`nombrePersona`, `apellidoPersona`, `dniPersona`, `telefonoPersona`,
+    // inserccion de los datos en la tabla de personas
+    $sqlInsertarPersona = "INSERT INTO `Personas`(`nombrePersona`, `apellidoPersona`, `dniPersona`, `telefonoPersona`,
   `emailPersona`) VALUES (:nombrePersona, :apellidoPersona, :dniPersona, :telefonoPersona, :emailPersona)";
-  $statement = $conexion->prepare( $sqlInsertarPersona );
-  $statement->bindParam( ":nombrePersona", $nombrePersona);
-  $statement->bindParam( ":apellidoPersona", $apellidoPersona );
-  $statement->bindParam( ":dniPersona", $dniPersona );
-  $statement->bindParam( ":telefonoPersona", $telefonoPersona);
-  $statement->bindParam( ":emailPersona", $correoPersona );
-  $statement->execute();
-  $idPersonaInsertada = $conexion->lastInsertId();
-  // return $idPersonaInsertada;
-  // inserccion en la tabla de doctores
-  $sqlInsertarUsuario = "INSERT INTO `Usuarios`(`nombreUsuario`, `contrasenaUsuario`, `estadoUsuario`, `idTipoUsuario`, `idPersona`, `token`) VALUES (:nombreUsuario,MD5(:contrasena),'1',:tipoUsuario,:idPersona,'')";
-  $statemente = $conexion->prepare( $sqlInsertarUsuario );
-  $statemente->bindParam( ":nombreUsuario", $nombreUsuario );
-  $statemente->bindParam( ":contrasena", $contrasenaUsuario );
-   $statemente->bindParam( ":tipoUsuario", $tipoUsuario );
-    $statemente->bindParam( ":idPersona", $idPersonaInsertada ); 
-  $statemente->execute();
+    $statement = $conexion->prepare( $sqlInsertarPersona );
+    $statement->bindParam( ":nombrePersona", $nombrePersona );
+    $statement->bindParam( ":apellidoPersona", $apellidoPersona );
+    $statement->bindParam( ":dniPersona", $dniPersona );
+    $statement->bindParam( ":telefonoPersona", $telefonoPersona );
+    $statement->bindParam( ":emailPersona", $correoPersona );
+    $statement->execute();
+    $idPersonaInsertada = $conexion->lastInsertId();
 
+    // inserccion en la tabla de doctores
+    $sqlInsertarDoctor = "INSERT INTO `Doctores`(`idPersona`, `idEspecialidad`) VALUES (:idPersona, 3)";
+    $statemente = $conexion->prepare( $sqlInsertarDoctor );
 
-     echo json_encode(true);
+    $statemente->bindParam( ":idPersona", $idPersonaInsertada );
+    $statemente->execute();
 
-}else{
-      
-      echo json_encode(false);
-      
-      }
-    
-      ?>
+    // inserccion en la tabla de doctores
+    $sqlInsertarUsuario = "INSERT INTO `Usuarios`(`nombreUsuario`, `contrasenaUsuario`, `estadoUsuario`, `idTipoUsuario`, `idPersona`, `token`) VALUES (:nombreUsuario,MD5(:contrasena),'1',:tipoUsuario,:idPersona,'')";
+    $statemente = $conexion->prepare( $sqlInsertarUsuario );
+    $statemente->bindParam( ":nombreUsuario", $nombreUsuario );
+    $statemente->bindParam( ":contrasena", $contrasenaUsuario );
+    $statemente->bindParam( ":tipoUsuario", $tipoUsuario );
+    $statemente->bindParam( ":idPersona", $idPersonaInsertada );
+    $statemente->execute();
+
+    echo json_encode( true );
+
+} else {
+
+    echo json_encode( false );
+
+}
+
+?>
